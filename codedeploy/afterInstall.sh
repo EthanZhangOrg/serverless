6c9c49a63fb0bb9d5dd99c1126cd0129f5d6ba6b
Ethan Zhang <ethanzhang1997@gmail.com>
2021-12-01 09:19:37 -0500

    1

--- codedeploy/afterInstall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# change ownership of the artifact
-sudo chown ubuntu:ubuntu /home/ubuntu/webapp_jar/webapp-0.0.1-SNAPSHOT.jar
--- codedeploy/beforeInstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# remove the old version of the webapp artifact as .jar
-sudo rm -rf /home/ubuntu/webapp_jar/webapp-0.0.1-SNAPSHOT.jar
-
-sudo rm -rf /opt/cloudwatch-config.json
